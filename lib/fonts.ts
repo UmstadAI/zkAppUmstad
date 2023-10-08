4066f72c2a51eab49dfe30d6c80810fd7c7d500e
berkingurcan <berkingurcan@yandex.com>
2023-10-08 20:21:23 +0300

    font titillium

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Roboto_Mono as FontMono, Roboto as FontSans} from 'next/font/google'
+import { Roboto_Mono as FontMono, Titillium_Web as FontSans} from 'next/font/google'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
