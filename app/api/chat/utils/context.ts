47373f9134a27ffcbfa49dfba5d904392ef37475
berkingurcan <berkingurcan@yandex.com>
2023-11-07 22:33:04 +0300

    update get vars

--- app/api/chat/utils/context.ts
+++ app/api/chat/utils/context.ts
@@ -9,16 +9,17 @@ export type Metadata = {
 }
 
 // The function `getContext` is used to retrieve the context of a given message
-export const getContext = async (message: string, namespace: string, maxTokens = 10000, minScore = 0.7, getOnlyText = true): Promise<string | ScoredPineconeRecord[]> => {
+export const getContext = async (message: string, namespace: string, maxTokens = 12000, minScore = 0.7, getOnlyText = true): Promise<string | ScoredPineconeRecord[]> => {
 
   // Get the embeddings of the input message
   const embedding = await getEmbeddings(message);
 
   // Retrieve the matches for the embeddings from the specified namespace
-  const matches = await getMatchesFromEmbeddings(embedding, 25, namespace);
+  const matches = await getMatchesFromEmbeddings(embedding, 31, namespace);
 
   // Filter out the matches that have a score lower than the minimum score
   const qualifyingDocs = matches.filter(m => m.score && m.score > minScore);
+  console.log(qualifyingDocs)
 
   if (!getOnlyText) {
     // Use a map to deduplicate matches by URL
