3c0f7a38f94378c475e2b3c8b7948704ffeb9c4a
berkingurcan <berkingurcan@yandex.com>
2023-11-10 12:55:47 +0300

    change prompt

--- app/api/chat/prompts.ts
+++ app/api/chat/prompts.ts
@@ -21,13 +21,13 @@ export function setPromtWithContext(codeCtx: any, ctx: any, projectCtx: any) {
     Forget about Solidity Smart Contracts
     AI Assistant is expert MINA Protocol AI Assistant and AI assistant is assisting developers about zkApps, o(1)js, zkSnarks, MINA smart contracts.
     Please provide a code example when the user requests code example by considering CODE_CONTEXT Block and use the information in CONTEXT Block use o1js for writing smart contracts.
-    Carefully understand code examples within the CODE_CONTEXT.
+    Carefully understand code examples within the CODE_CONTEXT BLOCK.
     Do not forget to consider MINA zkApp smart contracts are actually circuits.
 
     After that evaulate the PROJECT_CONTEXT Block for code and zkApp project examples
     if they include snarkyjs library consider it is renamed with o1js. 
     If you need some additional functionality take them from PROJECT_CONTEXT Block and apply to code examples.
-    However, PROJECT_CONTEXT Block can include outdated code examples. Consider smart contract structure from firstly CODE_CONTEXT.
+    However, PROJECT_CONTEXT Block can include outdated code examples. Consider smart contract structure and methods from firstly CODE_CONTEXT BLOCK and CONTEXT BLOCK.
 
     START CODE_CONTEXT BLOCK
     ${codeCtx}
