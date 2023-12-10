217ee3b66500a139f2c909f0758dafa1495c1040
berkingurcan <berkingurcan@yandex.com>
2023-12-10 16:36:44 +0700

    add is test

--- cli/zkappumstad/tools/writer_tool.py
+++ cli/zkappumstad/tools/writer_tool.py
@@ -4,7 +4,7 @@ from zkappumstad.utils import fade_in_text
 
 function_description = {
     "name": "write_code",
-    "description": "Always run this function after using other functions. Always ask for consent from the user to write file. This tool automates the generation of TypeScript files for zkApp projects. Use this function when AI wants to generate codes for existing zkApps project such as smart contracts and their tests or user wants to write by you. The generated code aligns with the user's specifications for smart contract functionalities within the zkApps framework. This tool is ideal for developers seeking to streamline their zkApp development process, ensuring rapid prototyping and consistency in code structure. Always ask for consent to write file",
+    "description": "Always run this function after using other functions. Always ask for consent from the user to write file. This tool automates the generation of TypeScript files for zkApp projects and their tests. Use this function when AI wants to generate codes for existing zkApps project such as smart contracts and their tests or user wants to write by you. The generated code aligns with the user's specifications for smart contract functionalities within the zkApps framework. This tool is ideal for developers seeking to streamline their zkApp development process, ensuring rapid prototyping and consistency in code structure.",
     "parameters": {
         "type": "object",
         "properties": {
@@ -16,6 +16,10 @@ function_description = {
                 "type": "string",
                 "description": "The typescript code for zkApps smart contracts and their tests to write.",
             },
+            "is_test": {
+                "type": "boolean",
+                "description": "true if the code is smart contract test false if it is not test code"
+            } 
         },
         "required": ["code"],
     },
@@ -24,17 +28,19 @@ function_description = {
 function_messages = "Writing code to file...\n"
 
 basedir = "initial_project/contracts/src"
-def run_tool(contract_name, code):
+def run_tool(contract_name, code, is_test):
     try:
-        filename = f"{contract_name}.ts" 
+        if is_test:
+            filename = f"{contract_name}.test.ts"
+        else:
+            filename = f"{contract_name}.ts"
         file_path = os.path.join(basedir, filename)
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
         with open(file_path, 'w') as file:
             file.write(code)
-            fade_in_text(f"Code written successfully to {file_path}", "bold green")
-
+            fade_in_text(f"Code written successfully to {file_path}!", "bold green")
 
         return code
 
