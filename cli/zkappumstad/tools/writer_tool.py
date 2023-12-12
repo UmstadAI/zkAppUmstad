a0d722b028221fbac9bdcc18f7c522c60fe399fd
berkingurcan <berkingurcan@yandex.com>
2023-12-12 15:51:30 +0700

    updates

--- cli/zkappumstad/tools/writer_tool.py
+++ cli/zkappumstad/tools/writer_tool.py
@@ -4,7 +4,7 @@ from zkappumstad.utils import fade_in_text
 
 function_description = {
     "name": "write_code",
-    "description": "Always run this function after using other functions. This tool automates the generation of TypeScript files for zkApp projects and their tests. Use this function when AI wants to generate codes for existing zkApps project such as smart contracts and their tests or user wants to write by you. The generated code aligns with the user's specifications for smart contract functionalities within the zkApps framework. This tool is ideal for developers seeking to streamline their zkApp development process, ensuring rapid prototyping and consistency in code structure.",
+    "description": "Always run this function after using other functions. This tool automates the generation of files for zkApp projects and their tests. Use this function when AI wants to generate README files, code files for existing zkApps project such as smart contracts and their tests or user wants to write by you. The generated code aligns with the user's specifications for smart contract functionalities within the zkApps framework. This tool is ideal for developers seeking to streamline their zkApp development process, ensuring rapid prototyping and consistency in code structure.",
     "parameters": {
         "type": "object",
         "properties": {
@@ -16,13 +16,13 @@ function_description = {
                 "type": "string",
                 "description": "The typescript code for zkApps smart contracts and their tests to write. Or markdown code for readme file.",
             },
-            "file_type": {  # enum, CONTRACT, TEST, README
+            "file_type": {
                 "type": "string",
                 "enum": ["CONTRACT", "TEST", "README"],
                 "description": "The type of the file to write.",
             },
         },
-        "required": ["code", "is_test"],
+        "required": ["code", "file_type"],
     },
 }
 
@@ -32,7 +32,7 @@ basedir = "initial_project"
 basedir_src = "initial_project/contracts/src"
 
 
-def run_tool(contract_name="", code="", file_type="CONTRACT"):
+def run_tool(contract_name="", code="", file_type=""):
     try:
         if file_type == "TEST":
             filename = f"{contract_name}.test.ts"
