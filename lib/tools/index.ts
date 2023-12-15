49ed6c9023db9a94d3cf1c2f9a796f902a0377a5
berkingurcan <berkingurcan@yandex.com>
2023-12-15 12:20:52 +0700

    feat add code tool

--- lib/tools/index.ts
+++ lib/tools/index.ts
@@ -1,11 +1,12 @@
 import { RunnableToolFunction } from 'openai/lib/RunnableFunction'
 import { docTool, docToolRunnable } from './doc'
+import { codeTool, codeToolRunnable } from './code'
 import { Tool } from './tool'
 
 type ToolMap = {
   [key: string]: Tool
 }
 
-export const tools: Tool[] = [docTool]
+export const tools: Tool[] = [docTool, codeTool]
 
-export const runnables: RunnableToolFunction<any>[] = [docToolRunnable]
+export const runnables: RunnableToolFunction<any>[] = [docToolRunnable, codeToolRunnable]
