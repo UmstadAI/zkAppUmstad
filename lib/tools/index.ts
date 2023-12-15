7df19b3088ec0a835307b0384527e2083c77745b
berkingurcan <berkingurcan@yandex.com>
2023-12-15 13:30:19 +0700

    feat add issue tool

--- lib/tools/index.ts
+++ lib/tools/index.ts
@@ -2,12 +2,13 @@ import { RunnableToolFunction } from 'openai/lib/RunnableFunction'
 import { docTool, docToolRunnable } from './doc'
 import { codeTool, codeToolRunnable } from './code'
 import { projectTool, projectToolRunnable } from './project'
+import { issueTool, issueToolRunnable } from './issue'
 import { Tool } from './tool'
 
 type ToolMap = {
   [key: string]: Tool
 }
 
-export const tools: Tool[] = [docTool, codeTool, projectTool]
+export const tools: Tool[] = [docTool, codeTool, projectTool, issueTool]
 
-export const runnables: RunnableToolFunction<any>[] = [docToolRunnable, codeToolRunnable, projectToolRunnable]
+export const runnables: RunnableToolFunction<any>[] = [docToolRunnable, codeToolRunnable, projectToolRunnable, issueToolRunnable]
