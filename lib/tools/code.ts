3ab2f52118f756959e5643b69576759e0300c6d3
berkingurcan <berkingurcan@yandex.com>
2024-01-03 14:41:52 +0300

    Update code.ts

--- lib/tools/code.ts
+++ lib/tools/code.ts
@@ -36,7 +36,7 @@ async function formatResults(matches: ScoredPineconeRecord[]) {
   const results = []
   for (let i = 0; i < matches.length; i++) {
     const match = matches[i]
-    if ((match.score || 1) > 0.85) {
+    if ((match.score || 1) > 0.70) {
       const metadata = match.metadata as Metadata
       const title = metadata.text
       const text = metadata.text
