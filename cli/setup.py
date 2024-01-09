f2f8581a50f2620a3b2bd10f6b409ac152e719a5
berkingurcan <berkingurcan@yandex.com>
2024-01-09 22:48:14 +0300

    setup

--- cli/setup.py
+++ cli/setup.py
@@ -4,25 +4,25 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="zkappsumstadcli",
-    version="0.1.0",
+    name="zkumstad",
+    version="0.1.2",
     author="Berkin Gurcan, Dogukan Akar",
     author_email="berkingurcan@gmail.com, dogukanakarc@gmail.com",
     description="CLI AI Agent for zkApps Developers",
-    long_description=open("./../README.md").read(),
+    long_description=open("./README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/UmstadAI/zkappumstad",
+    url="https://github.com/UmstadAI/zkAppUmstad/tree/main/cli",
     packages=find_packages(),
     install_requires=requirements,
     entry_points="""
         [console_scripts]
-        zkappumstad=zkappumstad:main
-        create=create_zk_project:main
+        zkumstad-start=zkappumstad:main
+        zkumstad-create=create_zk_project:main
     """,
     classifiers=[
-        'Topic :: AI Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Choose your license
-        'Programming Language :: Python :: 3',  # Specify which python versions you support
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
     ],
     python_requires='>=3.10', 
