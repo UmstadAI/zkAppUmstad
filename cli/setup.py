b56b18c1296e3c74609dcb34c14f022c8b182cd2
berkingurcan <berkingurcan@yandex.com>
2024-01-10 17:33:20 +0300

    Update setup.py

--- cli/setup.py
+++ cli/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="zkappumstad",
-    version="0.1.5",
+    version="0.1.6",
     author="Berkin Gurcan, Dogukan Akar",
     author_email="berkingurcan@gmail.com, dogukanakarc@gmail.com",
     description="CLI AI Agent for zkApps Developers",
