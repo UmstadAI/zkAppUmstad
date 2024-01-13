ffddfbf17e6f6731778589a5ef6213a4151e4622
berkingurcan <berkingurcan@yandex.com>
2024-01-13 14:54:41 +0300

    increase issue context

--- lib/tools/issue.ts
+++ lib/tools/issue.ts
@@ -36,7 +36,7 @@ async function formatResults(matches: ScoredPineconeRecord[]) {
   const results = []
   for (let i = 0; i < matches.length; i++) {
     const match = matches[i]
-    if ((match.score || 1) > 0.50) {
+    if ((match.score || 1) > 0.35) {
       const metadata = match.metadata as Metadata
       const title = metadata.text
       const text = metadata.text
@@ -49,7 +49,7 @@ async function formatResults(matches: ScoredPineconeRecord[]) {
 
 async function runTool(args: { query: string }): Promise<string> {
   const embeddings = await getEmbeddings(args.query)
-  const matches = await getMatchesFromEmbeddings(embeddings, 2, VECTOR_TYPE)
+  const matches = await getMatchesFromEmbeddings(embeddings, 3, VECTOR_TYPE)
 
   return formatResults(matches)
 }
