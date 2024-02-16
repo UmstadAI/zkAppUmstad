76331420616b858461d98fe96c8fe878580f1e11
dogukan <dogukanakarc@gmail.com>
2024-02-16 22:23:08 +0100

    fix #40

--- components/clear-history.tsx
+++ components/clear-history.tsx
@@ -57,12 +57,11 @@ export function ClearHistory({ clearChats }: ClearHistoryProps) {
                   toast.error(result.error)
                   return
                 }
-
-                setOpen(false)
-                router.push('/')
               } catch (error) {
                 console.error('Error during clear history:', error)
               }
+              setOpen(false)
+              router.push('/')
             }}
           >
             {isPending && <IconSpinner className="mr-2 animate-spin" />}
