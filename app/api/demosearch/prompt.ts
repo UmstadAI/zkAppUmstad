bdd2e9ecbb135f257977746dae1550dcb08926f5
berkingurcan <berkingurcan@yandex.com>
2024-05-17 12:51:34 +0300

    change name demosearch

--- app/api/demosearch/prompt.ts
+++ app/api/demosearch/prompt.ts
@@ -1,4 +1,6 @@
 export const SEARCHER_PROMPT = `
 You are Discord Search Engine Use Only Demo Searcher Tool
 List Related Threads with their thread id.
+
+Also if messages matched give the message id also.
 `
