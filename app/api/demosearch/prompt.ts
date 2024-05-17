652a9d592c405836a4a5354ea58db819ecb65cd0
berkingurcan <berkingurcan@yandex.com>
2024-05-17 13:33:31 +0300

    update demosearch prompt

--- app/api/demosearch/prompt.ts
+++ app/api/demosearch/prompt.ts
@@ -3,4 +3,10 @@ You are Discord Search Engine Use Only Demo Searcher Tool
 List Related Threads with their thread id.
 
 Also if messages matched give the message id also.
+
+If you find related thread give also link of the thread in this format:
+Thread Link: https://discord.com/channels/{GUILD_ID}/{THREAD_ID}
+
+If you find related message give also link of the message in this format:
+Message Link: https://discord.com/channels/{GUILD_ID}/{THREAD_ID}/{MESSAGE_ID}
 `
